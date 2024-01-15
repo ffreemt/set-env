89c4ebb99ae65d008130b1833a9fd2a9829e8c4b
ffreemt <yucongo+fmt@gmail.com>
2024-01-15 12:06:09 +0800

    Update verbose, pytest tests

--- src/set_env/__init__.py
+++ src/set_env/__init__.py
@@ -6,5 +6,6 @@ def hello():
     print("Hello from set-env!!")
     return "Hello from set-env!"
 
-__version__ = "0.1.0"
+
+__version__ = "0.1.1"
 __all__ = ("set_env",)
--- src/set_env/set_env.py
+++ src/set_env/set_env.py
@@ -3,11 +3,15 @@
 !pip install -q python-dotenv
 """
 import os
+import sys
 
 from dotenv import dotenv_values, find_dotenv
+from loguru import logger
 
 
-def set_env(env_var="HF_TOKEN", source_var=None, envfile=None, override=False):
+def set_env(
+    env_var="HF_TOKEN", source_var=None, envfile=None, override=False, verbose=False
+):
     """
     Set environ var via google userdat/kaggle_secrets/[.env/dotenv/env].
 
@@ -22,14 +26,22 @@ def set_env(env_var="HF_TOKEN", source_var=None, envfile=None, override=False):
         for ['.env', 'dotenv', 'env']
     override:
         reset if set to True
+    verbose:
+        for loguru.logging, print TRACE if set
 
     Returns:
     -------
     value of the env var.
     """
+    if verbose:
+        logger.remove()
+        logger.add(sys.stderr, level="TRACE")  # default "DEBUG"
+
     if override:
         if os.getenv(env_var) is not None:
+            save_env_var = os.environ[env_var]
             del os.environ[env_var]
+        logger.trace(f"{override=}")
 
     if source_var is None:
         source_var = env_var
@@ -38,6 +50,7 @@ def set_env(env_var="HF_TOKEN", source_var=None, envfile=None, override=False):
 
         try:
             os.environ[env_var] = userdata.get(source_var)
+            logger.trace(f"colab: set {env_var}={source_var}")
         except (userdata.SecretNotFoundError, userdata.NotebookAccessError):
             ...
     except ModuleNotFoundError:
@@ -53,6 +66,7 @@ def set_env(env_var="HF_TOKEN", source_var=None, envfile=None, override=False):
         user_secrets = UserSecretsClient()
         try:
             os.environ[env_var] = user_secrets.get_secret(source_var)  # BackendError
+            logger.trace(f"kaggle: set {env_var}={source_var}")
         except kaggle_web_client.BackendError:
             ...
     except ModuleNotFoundError:
@@ -66,6 +80,7 @@ def set_env(env_var="HF_TOKEN", source_var=None, envfile=None, override=False):
     if envfile is None:
         for _ in [".env", "dotenv", "env"]:
             envfile = find_dotenv(_)
+            logger.trace(f"Found {envfile=}")
             if envfile:
                 break
 
@@ -73,9 +88,22 @@ def set_env(env_var="HF_TOKEN", source_var=None, envfile=None, override=False):
         print(f"loading {envfile=} with dotenv_values(envfile)")
         if dotenv_values(envfile).get(source_var):
             os.environ[env_var] = dotenv_values(envfile).get(source_var)
-
+            logger.trace(f"{envfile=}: set {env_var}={source_var}")
     if os.getenv(env_var):
         return os.getenv(env_var)
 
     # try manual input?
+    logger.warning(
+        f"""
+        Unable to set {env_var}={source_var},
+        not in colab or Secret not set, not kaggle
+        or Secrets not set, no .env/dotenv/env file
+        in the current working dir or parent dirs."""
+    )
+
+    # restore
+    if override:
+        if os.getenv(env_var) is not None:
+            os.environ[env_var] = save_env_var
+
     return None
